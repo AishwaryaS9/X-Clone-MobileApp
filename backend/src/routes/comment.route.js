8580cf42318a70dc8633d74b12cfe4c80202668d
AishwaryaS9 <aishwaryasudupi9@gmail.com>
2025-07-23 19:37:10 +0530

    notifications and arcjet added

--- backend/src/routes/comment.route.js
+++ backend/src/routes/comment.route.js
@@ -2,7 +2,6 @@ import express from 'express'
 import { protectRoute } from '../middleware/auth.middleware.js';
 import { createComment, deleteComment, getComments } from '../controllers/comment.controller.js';
 
-
 const router = express.Router();
 
 router.get("/post/:postId", getComments);
