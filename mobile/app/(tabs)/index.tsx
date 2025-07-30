c9221a3993108c2bc6b6868d217bcde2dd987abd
AishwaryaS9 <aishwaryasudupi9@gmail.com>
2025-07-30 11:52:21 +0530

    feat: Added notifications functionality

--- mobile/app/(tabs)/index.tsx
+++ mobile/app/(tabs)/index.tsx
@@ -1,13 +1,24 @@
-import { ScrollView, Text, View } from 'react-native'
-import React from 'react'
+import { RefreshControl, ScrollView, Text, View } from 'react-native'
+import React, { useState } from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import SignOutButton from '@/components/SignOutButton'
 import { useUserSync } from '@/hooks/useUserSync'
 import { Ionicons } from '@expo/vector-icons'
 import PostComposer from '@/components/PostComposer'
 import PostsList from '@/components/PostsList'
+import { usePosts } from '@/hooks/usePosts'
 
 const HomeScreen = () => {
+    const [isRefetching, setIsRefetching] = useState(false);
+    const { refetch: refetchPosts } = usePosts();
+
+    const handlePullToRefresh = async () => {
+        setIsRefetching(true);
+
+        await refetchPosts();
+        setIsRefetching(false);
+    }
+
     useUserSync();
     return (
         <SafeAreaView className='flex-1 bg-white'>
@@ -17,7 +28,13 @@ const HomeScreen = () => {
                 <SignOutButton />
             </View>
             <ScrollView showsVerticalScrollIndicator={false}
-                className='flex-1' contentContainerStyle={{ paddingBottom: 80 }}>
+                className='flex-1' contentContainerStyle={{ paddingBottom: 80 }}
+                refreshControl={
+                    <RefreshControl refreshing={isRefetching}
+                    onRefresh={handlePullToRefresh}
+                    tintColor={"#1DA1F2"}
+                    />
+                }>
                 <PostComposer />
                 <PostsList />
             </ScrollView>
