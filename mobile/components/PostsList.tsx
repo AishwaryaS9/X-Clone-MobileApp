64394a18ab548ba35209ab47760fdd0a1c151155
AishwaryaS9 <aishwaryasudupi9@gmail.com>
2025-07-30 10:43:59 +0530

    feat: Comment functionality added

--- mobile/components/PostsList.tsx
+++ mobile/components/PostsList.tsx
@@ -1,13 +1,20 @@
 import { View, Text, ActivityIndicator, TouchableOpacity } from 'react-native'
-import React from 'react'
+import React, { useState } from 'react'
 import { useCurrentUser } from '../hooks/useCurrentUser'
 import { usePosts } from '@/hooks/usePosts';
 import { Post } from '@/types';
 import PostCard from './PostCard';
+import CommentsModal from './CommentsModal';
 
 const PostsList = () => {
     const { currentUser } = useCurrentUser();
     const { posts, isLoading, error, refetch, toggleLike, deletePost, checkIsLiked } = usePosts();
+    const [selectedPostId, setSelectedPostId] = useState<string | null>(null);
+
+    const selectedPost = selectedPostId ? posts.find((p: Post) => p._id === selectedPostId) : null;
+
+
+
 
     if (isLoading) {
         return (
@@ -44,11 +51,15 @@ const PostsList = () => {
                     post={post}
                     onLike={toggleLike}
                     onDelete={deletePost}
+                    onComment={(post: Post) => setSelectedPostId(post._id)}
                     currentUser={currentUser}
                     isLiked={checkIsLiked(post.likes, currentUser)}
 
                 />
             ))}
+
+            <CommentsModal selectedPost={selectedPost}
+                onClose={() => setSelectedPostId(null)} />
         </>
     )
 }
